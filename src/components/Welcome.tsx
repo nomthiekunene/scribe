24def9c2ee7f2a99eba3fdcb682af9b12a8b8f47
Nomthandazo <nothandazokunene290@gmail.com>
2025-09-23 16:47:26 +0200

    welcome

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,26 +1,10 @@
 import React from "react";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
-interface Post{
-  id: string;
-  title: string;
-  subtitle: string;
-  description: string;
-  date: string;
-  imageUrl?: string;
-}
-
-const getData = async () => {
-  const res = await fetch('http://localhost:3000/api/post',{cache:'no-store'});
-   if(!res.ok) return notFound()
-    return res.json()
-}
  
-const Welcome = async () => {
-   const data = await getData();
-   const posts:Post[]=Array.isArray(data)? data: data.posts || []
-
-
+const Welcome = () => {
+ 
   return (
     <div className="bg-black py-10 md:py-20">
       <div className="border-b-2 border-white pb-14 w-[100%] md:w-[80%] mx-auto">
@@ -33,6 +17,7 @@ const Welcome = async () => {
       <div className="flex px-20  mt-10 sm:mt-16 md:mt-20 text-center">
         <div className="flex flex-col md:flex-row justify-between items-center md:items-start w-[90%] md:w-[80%] mx-auto gap-8 md:gap-0">
           <h2 className="text-white text-2xl font-serif">Featured</h2>
+                  
           </div>
         <div className="flex  md:flex-row justify-between items-center md:items-start w-[90%] md:w-[80%] mx-auto gap-8 md:gap-0">
           <h2 className="text-white text-2xl font-serif">Recent</h2>
@@ -41,5 +26,6 @@ const Welcome = async () => {
     </div>
   );
 };
+
 
 export default Welcome;
